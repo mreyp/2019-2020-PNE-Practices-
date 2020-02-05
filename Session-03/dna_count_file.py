9b26412c737a8a03cccc770e307e31e3ce343217
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-02-05 17:50:27 +0100

    the error is in the second print, as values c= 3 and d= 3 creates a problem in t3, as t1 is 0 and you cannot divide by 0.

--- Session-03/dna_count_file.py
+++ Session-03/dna_count_file.py
@@ -1,22 +1,23 @@
-with open(dna.txt, r) as f:
-    counter = 0
-    a = 0
-    g = 0
-    c = 0
-    t = 0
-    for letter in f:
-        counter += 1
-        if letter == 'A':
-            a += 1
-        elif letter == 'G':
-            g += 1
-        elif letter == 'C':
-            c += 1
-        elif letter == 'T':
-            t += 1
+counter = 0
+a = 0
+g = 0
+c = 0
+t = 0
+with open('dna', 'r') as f:
+    for line in f:
+        for letter in line:
+            counter += 1
+            if letter == 'A':
+                a += 1
+            elif letter == 'G':
+                g += 1
+            elif letter == 'C':
+                c += 1
+            elif letter == 'T':
+                t += 1
 
-    print('Total length:', counter)
-    print('A:', a)
-    print('G:', g)
-    print('C:', c)
-    print('T:', t)
+print('Total length:', counter)
+print('A:', a)
+print('G:', g)
+print('C:', c)
+print('T:', t)
