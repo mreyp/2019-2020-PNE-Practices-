1b0bf3c94a567e444b6fa22a1d0f4845e608ae65
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-02-19 19:17:43 +0100

    exs

--- P1/Ex10.py
+++ P1/Ex10.py
@@ -1,17 +1,17 @@
-from Seq0 import *
+
 from Seq1 import Seq
-print("-----| Practice 1, Exercise 9 |------")
+print("-----| Practice 1, Exercise 10 |------")
 folder = "../Session-04/"
-filename = folder + 'U5.txt'
-s = Seq()
-s.read_fasta(filename)
+
+
 
 bases = [ 'A', 'C', 'T', 'G']
 genes = ['U5', 'ADA', 'FRAT1','FXN', 'RNU6_269P']
 
 for file in genes:
-    sequence = seq_read_fasta(folder + file + '.txt')
-    dict_bases = seq_count(sequence)
+    s = Seq()
+    sequence = s.read_fasta(folder + file + ".txt")
+    dict_bases = s.count()
     min_value = 0
     greater_number_base = ''
     for base, value in dict_bases.items():
