c4f8339f07744cf2280d3bfe93821b6ab976411c
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-02-25 18:07:45 +0100

    session 8

--- Session-08/IPs.py
+++ Session-08/IPs.py
@@ -1,3 +1,5 @@
-nc "Teacher's IP" 212.128.253.128
-
-
+Machine 	    Interface 	    Date 	    Place 	    IP
+mobile          Wifi 			25/02/2020  home
+mobile          Wifi            25/02/2020  URJC        10.3.34.19
+Lab computer 	Wire 			25/02/2020  URJC        212.128.253.135
+..... 	..... 	..... 	.......... 	..............
