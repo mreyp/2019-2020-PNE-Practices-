bf081729caa4c8482b0311b619c79932175eefa3
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-02-26 16:09:46 +0100

    practice 2

--- P2/Client0.py
+++ P2/Client0.py
@@ -15,24 +15,27 @@ class Client:
         return "Connection to SERVER at "+ self.ip+ ", PORT: "+ str(self.port)
 
     def talk(self, msg):
-
         # -- Create the socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         # establish the connection to the Server (IP, PORT)
         s.connect((self.ip, self.port))
-
         # Send data.
-        s.send(str.encode(msg))
+        s.send(str.encode(str(msg)))
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+        # Close the socket
+        s.close()
+        # Return the response
+        return response
 
     def debug_talk(self, msg):
         message = str(msg)
         response = self.talk(msg)
 
         print("To sever: ", end="")
-        termcolor.cprint(message, "green")
-
-        print("From sever: ", end="")
         termcolor.cprint(message, "blue")
+
+        print("From sever:\n ")
+        termcolor.cprint(response, "green")
 
 
