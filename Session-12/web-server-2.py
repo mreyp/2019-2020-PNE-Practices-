251536c21088f0455401b55cf088dcbdb30847e9
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-03-11 12:44:13 +0100

    session13

--- Session-12/web-server-2.py
+++ Session-12/web-server-2.py
@@ -1,16 +1,14 @@
-import socket
-import termcolor
+    import socket
+    import termcolor
 
+    # -- Server network parameters
+    IP = "127.0.0.1"
+    PORT = 8080
 
-# -- Server network parameters
-IP = "127.0.0.1"
-PORT = 8080
-
-
-def process_client(s):
-    # -- Receive the request message
-    req_raw = s.recv(2000)
-    req = req_raw.decode()
+    def process_client(s):
+        # -- Receive the request message
+        req_raw = s.recv(2000)
+        req = req_raw.decode()
 
     print("Message FROM CLIENT: ")
 
