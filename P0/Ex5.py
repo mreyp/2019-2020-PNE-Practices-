da9334b3cfd6fa2418b0a8e5d454f20db55d98b8
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-03-30 18:04:13 +0200

    P0 exercise 5

--- P0/Ex5.py
+++ P0/Ex5.py
@@ -1,9 +1,10 @@
 from Seq0 import *
+
 print('-----| Exercise 5 |------')
 
 folder = "../Session-04/"
-bases = [ 'A', 'C', 'T', 'G']
-genes = ['U5', 'ADA', 'FRAT1','FXN', 'RNU6_269P']
+bases = ['A', 'C', 'T', 'G']
+genes = ['U5', 'ADA', 'FRAT1', 'FXN', 'RNU6_269P']
 
 for file in genes:
     sequence = seq_read_fasta(folder + file + '.txt')
