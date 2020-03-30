e50565327f9defc9a4255f3c22b4529f3cc319d6
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-03-30 18:06:50 +0200

    P0 exercise 6

--- P0/Ex6.py
+++ P0/Ex6.py
@@ -3,7 +3,9 @@ from Seq0 import *
 print('------| Exercise 6 |------')
 folder = "../Session-04/"
 filename = 'U5.txt'
+
 sequence = seq_read_fasta(folder + filename)
+
 
 print('Gene ' + filename + ':')
 print('Frag: ' + (sequence[:20]))
