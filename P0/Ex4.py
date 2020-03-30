adcf51834b4a8d696788b00228f1dd81dd2e297c
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-03-30 18:03:21 +0200

    P0 exercise 4

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -6,9 +6,9 @@ folder = "../Session-04/"
 bases = ['A', 'C', 'T', 'G']
 genes = ['U5', 'ADA', 'FRAT1', 'FXN', 'RNU6_269P']
 
-for file in genes:
-    sequence = seq_read_fasta(folder + file + '.txt')
-    print('Gene ' + file + ':')
+for gene in genes:
+    sequence = seq_read_fasta(folder + gene + '.txt')
+    print('Gene ' + gene + ':')
 
     for base in bases:
         print(base + ': ', seq_count_base(sequence, base))
