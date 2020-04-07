01e16856589f0e13aae7f35fdf206f4510173f47
mreyp <m.reyp.2019@alumnos.urjc.es>
2020-04-07 18:02:22 +0200

    session 10

--- Session-10/happy-server.py
+++ Session-10/happy-server.py
@@ -41,8 +41,7 @@ while True:
         print("A client has connected to the server!")
 
         # Step 5: receiving information from the server (recv and decode)
-        # -- Read the message from the client
-        # -- The received message is in raw bytes
+
         msg_raw = cs.recv(2048)
 
         # -- We decode it for converting it into a human-redeable string
